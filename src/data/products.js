0c7a26d8b192b864497c0e5631a854f34f50448b
adityaraj28491-coder <adityaraj28491@gmail.com>
2025-09-17 19:33:52 +0530

    Create products.js

---
+++ src/data/products.js
@@ -0,0 +1,22 @@
+const products = [
+  {
+    id: 1,
+    name: "T-shirt - Blue",
+    price: 499,
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 2,
+    name: "Denim Jeans",
+    price: 999,
+    image: "https://via.placeholder.com/150"
+  },
+  {
+    id: 3,
+    name: "Sneakers - White",
+    price: 1499,
+    image: "https://via.placeholder.com/150"
+  }
+];
+
+export default products;
