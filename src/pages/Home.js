905d11abc0358499c3998bd846dc465dd49fed68
adityaraj28491-coder <adityaraj28491@gmail.com>
2025-09-17 19:32:39 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import products from "../data/products";
+import ProductCard from "../components/ProductCard";
+
+function Home() {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+    alert(`${product.name} added to cart!`);
+  };
+
+  return (
+    <div className="product-list">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} addToCart={addToCart} />
+      ))}
+    </div>
+  );
+}
+
+export default Home;
