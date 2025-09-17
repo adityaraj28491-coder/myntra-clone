69d1e861ddb5928207e214dd877ab00260d15938
adityaraj28491-coder <adityaraj28491@gmail.com>
2025-09-17 19:33:12 +0530

    Create Cart.js

---
+++ src/pages/Cart.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Cart() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h2>Cart Page (Coming Soon)</h2>
+      <p>This is a placeholder for the cart functionality.</p>
+    </div>
+  );
+}
+
+export default Cart;
